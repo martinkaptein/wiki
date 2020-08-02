ff39ed786f98dd61514d8eaf9a65a26d5542b7f9
Martin Kaptein <14858887+martinkaptein@users.noreply.github.com>
2020-08-02 14:59:04 +0200

    easy switch between tty and graphical systemd

--- configs/config.fish
+++ configs/config.fish
@@ -1,4 +1,6 @@
 alias t="Thunar ."
 alias e="vim ."
-alias f="openscript"
+alias f="open"
 alias v="vim"
+alias switchtty="systemctl stop graphical.target; systemctl start multi-user.target"
+alias switchgraphic="systemctl stop multi-user.target; systemctl start graphical.target"
