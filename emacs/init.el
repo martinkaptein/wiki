ab2ff0c87595a1ef83e7e57227a3e7973c151339
martinkaptein <martinkaptein242@gmail.com>
2021-11-27 22:29:31 +0100

    emacs as org main

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-battery-mode t)
+ '(display-time-mode t)
+ '(global-display-line-numbers-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 (load-theme 'tango-dark)
 (require 'org)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -16,3 +30,11 @@
 (setq org-agenda-files '("~/inbox.org"))
 (setq ispell-program-name "/usr/local/bin/aspell")
 (add-to-list 'exec-path "/usr/local/bin/")
+
+
+;; org mode optimized
+(setq inhibit-splash-screen t)
+(org-agenda-list)
+(delete-other-windows)
+
+;; keys
