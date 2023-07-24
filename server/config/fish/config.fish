978863349ff1e4e443013129ef8ed417ebf8367a
Martin Kaptein <martin@martinkaptein.com>
2023-07-24 10:15:28 +0200

    default commit message

---
+++ server/config/fish/config.fish
@@ -0,0 +1,6 @@
+alias tsm="transmission-remote"
+alias v="nvim"
+alias s="systemctl"
+alias j="journalctl -xe"
+alias buildsonata8='cd /root/github/sonata8 && git pull && hugo --destination /var/sites/sonata8/'
+alias occ="sudo -u 'www-data' /usr/bin/php8.2 /var/www/nextcloud/occ"
